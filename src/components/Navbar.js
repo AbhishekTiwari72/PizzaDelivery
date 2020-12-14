e870d2178bade34efa85fe422d8f0a9449db0669
Abhishek Kumar Tiwari <53769202+AbhishekTiwari72@users.noreply.github.com>
2020-12-14 13:11:47 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,49 @@
+import React from "react";
+import Login from './Login';
+//import Form from 'Form';
+const Navbar = () => {
+    return ( <
+        nav className = "navbar" >
+        <
+        div className = "navbar__text" >
+        <
+        div className = "logo" >
+        <
+        img src = "/img/logo.png"
+        alt = "logo" / >
 
+        <
+        /div> <
+        ul className = "navbar__ul" >
+        <
+        li >
+        <
+        a href = "/" > Home < /a> < /
+        li > <
+        li >
+        <
+        a href = "/menu" > Menu < /a> < /
+        li > <
+        li >
+        <
+        a href = "/shop" > Shop < /a> < /
+        li > <
+        li >
+        <
+        a href = "/news" > News < /a> < /
+        li > <
+        li >
+        <
+        a href = "" > Contact < /a> < /
+        li > <
+        li >
+        <
+        a href = "/login" > Login < /a> < /
+        li > <
+        /ul> < /
+        div > <
+        /nav>
+    );
+};
+
+export default Navbar;
